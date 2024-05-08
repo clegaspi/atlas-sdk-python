d5affccc2aa6a64333a3b53393754b348fa9da04
Christian Legaspi <1392774+clegaspi@users.noreply.github.com>
2024-05-08 11:36:54 -0700

    Initial setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from atlas_sdk_python import __version__
+
+setup(
+    name="atlas-sdk-python",
+    version=__version__,
+    description="SDK for accessing MongoDB Cloud and Atlas APIs",
+    author="Christian Legaspi",
+    author_email="christian.legaspi@mongodb.com",
+    url="https://github.com/clegaspi/atlas-sdk-python",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+    ],
+)
